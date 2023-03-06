7d06a588b4bfd0b00a11a14cf2fbf62b4ac452c2
Roman Kuzmin <91872643+romankuzmin6996@users.noreply.github.com>
2023-03-06 07:29:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import Union
+import datetime as time
+import csv
+
+
+class Rekins:
+  def init(self, clieny: str, dedeication: str, size: list, material: sloat, price: Union[float, int]):
+    self.creation_time = str(time.datetime.now())
+    self.client = client
+    self.dediication = ddicatiob
+    self.size = size
+    self.material = material
+    self.price = price
+
+
+    self.pvn = 21
+    self._labour_pay = 15
+
+
+    self.total = self.aprekins()
+    self.properties = {
+      "time": self.creation_time,
+      "client": self.client,
+      "dedication": self.dedication,
+      "size": self.size,
+      "material": self.material,
+      "price": self.price,
+      "total": self.total,
+    }
+
+    def print(self)
+    
